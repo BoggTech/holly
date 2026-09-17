d4eaad676f8502ea1e56896880318034b3acec3f
Darryl <darryl@bogg.tech>
2026-09-17 21:15:32 +0100

    rewrite welcome email

--- src/features/email-validation/send-verification-email.ts
+++ src/features/email-validation/send-verification-email.ts
@@ -7,7 +7,7 @@ import { sendGmail } from "./send-gmail.js";
 const DISCORD_INVITE_URL = process.env.DISCORD_INVITE_URL!;
 
 const STARTER_EMAIL_SUBJECT =
-  "Science Fiction & Fantasy Society Discord";
+  "Science Fiction & Fantasy Society Discord & Verification Code";
 
 const VERIFICATION_EMAIL_SUBJECT =
   "Your Science Fiction & Fantasy Society Discord verification code";
@@ -28,11 +28,13 @@ export async function sendStarterEmail(recipientEmail: string): Promise<void> {
 
 If you're receiving this email, it means you recently signed up for DU Science Fiction & Fantasy Society. Thanks, and we really hope you enjoy what we have in store this year!
 
-We have an active community over on Discord and, if you're interested, we'd love for you to join!
+We have an active community over on Discord and, if you're interested, we'd love for you to join! 
 ${DISCORD_INVITE_URL}
 
-For safety, all members of the server must confirm their TCD email. 
-When you join:
+If not, don't worry - joining our Discord server isn't required to participate in any events! You can find us elsewhere by following our Linktree:
+https://linktr.ee/sfsoc
+
+If you do join the Discord, follow these steps to verify your membership and get access to the full server:
  1. Navigate to the #verification channel.
  2. Click 'Enter Verification Code'.
  3. Enter the following code:
@@ -42,10 +44,7 @@ ${verificationCode}
 This code is tied to your email - do not share this code with anybody!
 It will expire in ${VERIFICATION_CODE_EXPIRY} - but you can always request a new one with the 'Resend Verification Code' button in the #verification channel.
 
-Hope to see you there!
-
-If Discord's not your style, you may find our linktree useful:
-https://linktr.ee/sfsoc
+Hope to see you around The Bunker!
 
 This is an automated email. You're receiving this because you recently purchased a DU Science Fiction & Fantasy membership under this email address.`;
 
